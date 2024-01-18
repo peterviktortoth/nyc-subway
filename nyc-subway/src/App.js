6f342acf65c0e03ac3e4c7e184d0bc4bfb98edff
peterviktortoth <152824719+peterviktortoth@users.noreply.github.com>
2024-01-17 22:09:52 -0500

    UI changes

--- nyc-subway/src/App.js
+++ nyc-subway/src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 
 function App() {
   const [transitData, setTransitData] = useState([]);
+  const [directionFilter, setDirectionFilter] = useState('all');
+
+  const handleDirectionChange = (event) => {
+    setDirectionFilter(event.target.value);
+  };
+
 
   const fetchData = () => {
     fetch('/refresh')
       .then(response => response.json())
       .then(data => {
+        console.log(data);
         setTransitData(data);
       })
       .catch(error => console.error('Error:', error));
@@ -20,10 +27,33 @@ function App() {
   }, []);
 
   return (
-    <div className="container">
-      <h1>Lexington Ave - 59th St</h1>
-      <div id="data-container">
-      {transitData.map((item, index) => {
+    <div>
+    <header className="header">
+      <h2>Lexington Avenue - 59th Street</h2>
+      {/* Any additional header content */}
+    </header>
+      <div className="tabs-container">
+        <div className="tabs">
+          {/* Tab radio buttons and labels */}
+          <input type="radio" id="radio-1" name="tab" value="all" checked={directionFilter === 'all'} onChange={handleDirectionChange} />
+          <label htmlFor="radio-1" className="tab">All</label>
+  
+          <input type="radio" id="radio-2" name="tab" value="uptown" checked={directionFilter === 'uptown'} onChange={handleDirectionChange} />
+          <label htmlFor="radio-2" className="tab">Uptown</label>
+  
+          <input type="radio" id="radio-3" name="tab" value="downtown" checked={directionFilter === 'downtown'} onChange={handleDirectionChange} />
+          <label htmlFor="radio-3" className="tab">Downtown</label>
+  
+          <div className="glider"></div>
+        </div>
+      </div>
+      <div className="data-container">
+      {transitData
+    .filter(item => 
+      (directionFilter === 'all' || item.message.toLowerCase().includes(directionFilter.toLowerCase())) &&
+      item.minutes_until_arrival >= 0 // Filter out negative arrival times
+    
+      ).map((item, index) => {
         const parts = item.message.split(' ');
         const trainLine = parts[1]; // Assuming the train line is always the second word
 
