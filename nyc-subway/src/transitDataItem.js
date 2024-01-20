1f6b5cc92fd92426a8b31178a13fbc6b671ec2bd
peterviktortoth <152824719+peterviktortoth@users.noreply.github.com>
2024-01-20 11:55:13 -0500

    commit

--- nyc-subway/src/transitDataItem.js
+++ nyc-subway/src/transitDataItem.js
@@ -3,12 +3,15 @@ import './App.css';
 
 const TransitDataItem = ({ item }) => {
     const getRectangleColor = (minutesUntilArrival) => {
-      if (minutesUntilArrival <= 5) {
-        return 'rgba(255, 0, 0, 0.5)';
-      } else {
-        return 'rgba(0, 255, 0, 0.5)';
-      }
-    };
+        if (minutesUntilArrival <= 5) {
+          return 'rgba(255, 0, 0, 0.2)'; // Red for 5 minutes or less
+        } else if (minutesUntilArrival <= 8) {
+          return 'rgba(255, 165, 0, 0.2)'; // Orange for 6 to 10 minutes
+        } else {
+          return 'rgba(0, 255, 0, 0.2)'; // Green for more than 15 minutes
+        }
+      };
+      
   
     const getColorClass = (trainLine) => {
         if (['4', '5', '6'].includes(trainLine)) {
@@ -27,6 +30,7 @@ const TransitDataItem = ({ item }) => {
         <p>
           {parts[0]} <span className={`train-line-indicator ${colorClass}`}>{trainLine}</span> {parts.slice(2,5).join(' ')}
           <span
+            className="bold-text" // Apply the bold-text class here
             style={{
               backgroundColor: rectangleColor,
               padding: '4px 4px',
